61e1b3cea7ea79a911ecd8379c425a743e1d16e2
shindepriti <pritishinde34@gmail.com>
2020-04-21 23:00:47 +0530

    ADDED:Pass The Negative Test case For 0 Feet Not Equal To 1 Feet

--- test/QuantityMeasurementTest.js
+++ test/QuantityMeasurementTest.js
@@ -5,4 +5,7 @@ describe('QuantityMeasurement',function(){
     it("given 0 feet and 0 feet should return equal",function(){
         assert.equal(quatityMesure.unitMeasurement(0,0),true);
     })
+    it("given 0 feet and 1 feet should return not equal",function () {
+        assert.notEqual(quatityMesure.unitMeasurement(0,1),true);
+    })
 });
