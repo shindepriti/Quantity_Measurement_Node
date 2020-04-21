8580489edbfc5dbf5dfe085e084783919a9b5097
shindepriti <pritishinde34@gmail.com>
2020-04-21 23:31:17 +0530

    ADDED:check Type Of Feet Value Using Typeof

--- app/QuantityMeasurement.js
+++ app/QuantityMeasurement.js
@@ -2,9 +2,14 @@ console.log("Wel-Come To Quantity Measurement")
 
 module.exports = {
 
-    unitMeasurement(unitValue1, unitValue2){
-        if(unitValue1 == unitValue2)
-            return true;
-        return false;
+    unitMeasurement(unitType, unitValue){
+        var unitTypes = ["feet","inch"];
+        var result,input = unitValue
+
+        if(unitTypes[0] == unitType)
+            result = 1*input;
+        if (unitTypes[1] == unitType)
+            result = 1*input;
+        return result;
     }
 }
--- test/QuantityMeasurementTest.js
+++ test/QuantityMeasurementTest.js
@@ -3,12 +3,15 @@ const quatityMesure = require('../app/QuantityMeasurement.js');
 
 describe('QuantityMeasurement',function(){
     it("given 0 feet and 0 feet should return equal",function(){
-        assert.equal(quatityMesure.unitMeasurement(0,0),true);
+        assert.equal(quatityMesure.unitMeasurement('feet',0),0);
     })
     it("given 0 feet and 1 feet should return not equal",function () {
         assert.notEqual(quatityMesure.unitMeasurement(0,1),true);
     })
     it("given 0 feet and null should return null",function(){
         assert.isNull(null,quatityMesure.unitMeasurement(0,null));
+    })
+    it("given feet value should return number",function(){
+        assert.typeOf(quatityMesure.unitMeasurement("feet",0),'Number');
     })
 });
