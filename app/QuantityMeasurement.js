7ee4ddf42aca8844b8a7af4712be9722a1ba0aa5
shindepriti <pritishinde34@gmail.com>
2020-04-22 01:31:19 +0530

    ADDED:Pass Test Case For Comparing 3 Feet Equal To 1 Yard

--- app/QuantityMeasurement.js
+++ app/QuantityMeasurement.js
@@ -2,20 +2,37 @@ console.log("Wel-Come To Quantity Measurement")
 
 module.exports = {
 
-    unitMeasurement(unitType, unitValue){
-        
-        var unitTypes = ["feet","inch"];
-        var result,input = unitValue
+    unitMeasurement(unitType1, unitType2, unitValue) {
+        var unitTypes = ["feet", "inch","yard"];
+        var result, input = unitValue;
 
-        if(unitTypes[0] == unitType){
+       if (unitTypes[0] == unitType1 && unitTypes[1] == unitType2) {
             result = 12 * input;
+            return result;
         }
-        else{
-            result = 1*input;
+        // if (unitTypes[1] == unitType1 && unitTypes[0] == unitType2) {
+        //     result = parseFloat(input / 12);
+        //     return result;
+        // }
+        if(unitType1==unitTypes[0] && unitType2==unitTypes[2]){
+            result=(input/3);
+            return result;
         }
-        if (unitTypes[1] == unitType){
-            result = 1*input;
-        }       
+        if(unitType1==unitTypes[2] && unitType2==unitTypes[0]){
+            result=input*3;
+            return result
+        }
+        if(unitType1==unitTypes[2] && unitType2==unitTypes[1]){
+            result=input*36;
+            return result;
+        }
+        if(unitType1==unitTypes[1] && unitType2==unitTypes[2]){
+            result=input/36;
+            return result;
+        }
+        else {
+            result = 1 * input;
+        }
         return result;
     }
 }
--- test/QuantityMeasurementTest.js
+++ test/QuantityMeasurementTest.js
@@ -3,36 +3,37 @@ const quatityMesure = require('../app/QuantityMeasurement.js');
 
 describe('QuantityMeasurement',function(){
     it("given 0 feet and 0 feet should return equal",function(){
-        assert.equal(quatityMesure.unitMeasurement('feet',0),0);
+        assert.equal(0,quatityMesure.unitMeasurement('feet','feet',0));
     })
     it("given 0 feet and 1 feet should return not equal",function () {
-        assert.notEqual(quatityMesure.unitMeasurement("feet",1),0);
+        assert.notEqual(0,quatityMesure.unitMeasurement("feet","feet",1));
     })
     it("given 0 feet and null should return null",function(){
-        assert.isNull(null,quatityMesure.unitMeasurement("feet",0));
+        assert.isNull(null,quatityMesure.unitMeasurement("feet","feet",0));
     })
     it("given feet value should return number",function(){
         assert.typeOf(quatityMesure.unitMeasurement("feet",0),'Number');
     })
     it("given 0 inch and 0 inch should return equal",function(){
-        assert.equal(quatityMesure.unitMeasurement('inch',0),0);
+        assert.equal(0,quatityMesure.unitMeasurement('inch','inch',0));
     })
     it("given 0 inch and 1 inch should return not equal",function () {
-        assert.notEqual(quatityMesure.unitMeasurement("inch",1),0);
+        assert.notEqual(0,quatityMesure.unitMeasurement("inch","inch",1));
     })
     it("given 0 inch and null should return null",function(){
         assert.isNull(null,quatityMesure.unitMeasurement("inch",0));
     })
     it("given inch value should return number",function(){
-        assert.typeOf(quatityMesure.unitMeasurement("inch",0),'Number');
+        assert.typeOf(quatityMesure.unitMeasurement("inch","inch",0),'number');
     })
     it(" given 1 feet should be equal to 12 inch",function(){
-        assert.equal(quatityMesure.unitMeasurement("feet",1),12);
+        assert.equal(12,quatityMesure.unitMeasurement("feet","inch",1));
     })
     it(" given 1 feet and 1 inch should return not equal",function(){
-        let checkFeet = quatityMesure.unitMeasurement("feet",1);
-        let checkInch = quatityMesure.unitMeasurement("inch",1);
-        assert.notEqual(checkFeet,checkInch);
+        assert.notEqual(1,quatityMesure.unitMeasurement("feet","inch",1));
     })
-   
+    it(" given 3 feet should be equal to 1 yard",function(){
+        assert.equal(1,quatityMesure.unitMeasurement("feet","yard",3));
+    })
+    
 });
