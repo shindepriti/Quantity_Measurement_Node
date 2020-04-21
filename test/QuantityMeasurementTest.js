d81bc8ea84dedfdb8a1a46ca52cbc471a0f29d9e
shindepriti <pritishinde34@gmail.com>
2020-04-22 00:17:42 +0530

    ADDED:Pass The Test case For 1 Inch And 1 Feet Return Not Equal

--- test/QuantityMeasurementTest.js
+++ test/QuantityMeasurementTest.js
@@ -9,7 +9,7 @@ describe('QuantityMeasurement',function(){
         assert.notEqual(quatityMesure.unitMeasurement("feet",1),0);
     })
     it("given 0 feet and null should return null",function(){
-        assert.isNull(null,quatityMesure.unitMeasurement(0,null));
+        assert.isNull(null,quatityMesure.unitMeasurement("feet",0));
     })
     it("given feet value should return number",function(){
         assert.typeOf(quatityMesure.unitMeasurement("feet",0),'Number');
@@ -21,7 +21,7 @@ describe('QuantityMeasurement',function(){
         assert.notEqual(quatityMesure.unitMeasurement("inch",1),0);
     })
     it("given 0 inch and null should return null",function(){
-        assert.isNull(null,quatityMesure.unitMeasurement(0,null));
+        assert.isNull(null,quatityMesure.unitMeasurement("inch",0));
     })
     it("given inch value should return number",function(){
         assert.typeOf(quatityMesure.unitMeasurement("inch",0),'Number');
@@ -29,6 +29,10 @@ describe('QuantityMeasurement',function(){
     it(" given 1 feet should be equal to 12 inch",function(){
         assert.equal(quatityMesure.unitMeasurement("feet",1),12);
     })
-   
+    it(" given 1 feet and 1 inch should return not equal",function(){
+        let checkFeet = quatityMesure.unitMeasurement("feet",1);
+        let checkInch = quatityMesure.unitMeasurement("inch",1);
+        assert.notEqual(checkFeet,checkInch);
+    })
    
 });
