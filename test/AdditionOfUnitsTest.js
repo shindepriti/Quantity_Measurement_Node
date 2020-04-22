9387023d95f1ea9654129c679fbd7da4b76b22dd
shindepriti <pritishinde34@gmail.com>
2020-04-22 15:45:02 +0530

    ADDED:Functionality To Add 1 Feet And 2 Inch Return 14 Inch

--- test/AdditionOfUnitsTest.js
+++ test/AdditionOfUnitsTest.js
@@ -4,6 +4,9 @@ const unitAddition = require('../app/QuantityMeasurement.js');
 describe("Additition of Two Units",function(){
 
     it("given 2 inch and 2 inch should return addition of length in inch",function(){
-        assert.equal(4,unitAddition.additionOfMeasurement("inch",2,"inch",2))
+        assert.equal(4,unitAddition.additionOfMeasurement("inch",2,"inch",2,"inch"))
+    })
+    it("given 1 feet and 2 inch should return 14 length in inch",function(){
+        assert.equal(14,unitAddition.additionOfMeasurement("feet",1,"inch",2,"inch"))
     })
 })
